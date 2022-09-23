83d0b7684b9b241df45ae86ec775fb5d6f55c4e5
Javokhirbek Nazarov <javohirnazarov99@gmail.com>
2022-09-23 11:43:01 -0500

    feature/WAA Init spring project

---
+++ src/main/java/uz/spiders/propertymanagement/PropertyManagementApplication.java
@@ -0,0 +1,13 @@
+package uz.spiders.propertymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/spiders/propertymanagement/PropertyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.spiders.propertymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
